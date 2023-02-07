f46838eb9bac5b51938c80fd8107e6183a874286
adam.awizen <awizen.adam@gmail.com>
2023-02-07 12:29:05 +0100

    green test whenThereAreNoBlocksReturnedOptionalShouldBeEmpty

--- src/main/java/com/horus/Wall.java
+++ src/main/java/com/horus/Wall.java
@@ -1,11 +1,16 @@
 package com.horus;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class Wall implements Structure {
 
     private List<Block> blocks;
+
+    public Wall() {
+        this.blocks = new ArrayList<>();
+    }
 
     @Override
     public Optional<Block> findBlockByColor(String color) {
